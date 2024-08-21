1ec7dd632740c5539b98837a9fb5392cad4903f0
Illia <dudchik931@gmail.com>
2024-08-21 17:21:42 +0300

    initializing router and server

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port is not found!")
+	}
 
-	fmt.Println("hello go!")
-	var input string
-	fmt.Scanln(&input)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	fmt.Println("Server started and running at port : ", portString,
+		"\nCtrl+C to stop manually")
+	error := srv.ListenAndServe()
+	if error != nil {
+		log.Fatal(error)
+	}
 }
